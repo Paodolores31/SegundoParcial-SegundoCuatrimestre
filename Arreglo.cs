fd53150ddce4d7f26f5f4afdc6dfdc85a2971832
Paodolores31 <pdolores471@gmail.com>
2026-03-21 05:15:39 -0600

    Add Program to fill and display an array of numbers.cs

---
+++ Arreglo.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+
+namespace Llenar_arreglo;
+
+public static class Program
+{
+    public static void Main()
+    {
+
+
+
+        int[] arreglo = new int[5];
+
+        Console.WriteLine("Por favor, ingresa 5 números:");
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Introduce el número para la posición {i}: ");
+
+            arreglo[i] = int.Parse(Console.ReadLine());
+        }
+
+
+        Console.WriteLine("¡Arreglo lleno! Los números son:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Posición {i}: {arreglo[i]}");
+        }
+    }
+}
+
+
+
+
+
+
+
